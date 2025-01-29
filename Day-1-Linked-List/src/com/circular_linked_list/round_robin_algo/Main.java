bfc988df70eb9672f4a7b9339b7d20d9f46ac887
masterujjval <guptarickey3@gmail.com>
2025-01-29 15:15:34 +0530

    Round Robin Algorithm Done

--- Day-1-Linked-List/src/com/circular_linked_list/round_robin_algo/Main.java
+++ Day-1-Linked-List/src/com/circular_linked_list/round_robin_algo/Main.java
@@ -3,9 +3,9 @@ package com.circular_linked_list.round_robin_algo;
 public class Main {
     public static void main(String[] args) {
         RoundRobin p=new RoundRobin();
-        p.addProcess(121,9,1);
-        p.addProcess(991,6,3);
-        p.addProcess(1091,9,5);
+        p.addProcess(121,3,7);
+        p.addProcess(991,3,3);
+        p.addProcess(1091,12,5);
         p.addProcess(2312,13,10);
 
         p.startProcess();
--- Day-1-Linked-List/src/com/circular_linked_list/round_robin_algo/RoundRobin.java
+++ Day-1-Linked-List/src/com/circular_linked_list/round_robin_algo/RoundRobin.java
@@ -44,38 +44,37 @@ public class RoundRobin {
 
 public void startProcess(){
         Node tail=head;
-        Node prev=head;
+        Node prev=temp;  // temp is tail
+        Node tester=prev;
         int t=counter;
     System.out.println("\nTime Quantum is: "+tq);
     System.out.println("\nStarting the process execution....\n");
         // turn around time
-        int tt=0;
+
 
         while(counter!=0){
             time+=(tail.burstTime)-tq;
             tail.burstTime= (tail.burstTime)-tq;
+
             if(counter==1){
                 tail.next=tail;
             }
 
             if(tail.burstTime<=0){
+                 // true for temp too as the last tail will get plunge in
+                    System.out.println("\nDeleting the process: " + tail.processId);
+                    counter--;
+                    prev.next = tail.next;
 
-
-
-                System.out.println("\nDeleting the process: "+tail.processId);
-                counter--;
-                prev.next=tail.next;
-
-
+                    tester=null;
             }
             else {
-
-
                 System.out.println("\nProcess Id: " + tail.processId + "\nBurst Time: " +tail.burstTime+"\nPriority: " + tail.priority);
-
-
             }
-            prev=tail;
+            if(tester!=null){
+                prev=tail;
+            }
+            tester=tail;
             tail=tail.next;
 
 

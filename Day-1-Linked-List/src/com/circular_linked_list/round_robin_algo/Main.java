e37d6d1323d99ac15af0cea73aba322b824e8d43
masterujjval <guptarickey3@gmail.com>
2025-01-29 12:10:54 +0530

    Round Robin Algorithm Done

---
+++ Day-1-Linked-List/src/com/circular_linked_list/round_robin_algo/Main.java
@@ -0,0 +1,16 @@
+package com.circular_linked_list.round_robin_algo;
+
+public class Main {
+    public static void main(String[] args) {
+        RoundRobin p=new RoundRobin();
+        p.addProcess(121,3,1);
+        p.addProcess(991,6,3);
+        p.addProcess(1091,9,5);
+        p.addProcess(2312,13,10);
+
+        p.startProcess();
+
+
+
+    }
+}
---
+++ Day-1-Linked-List/src/com/circular_linked_list/round_robin_algo/RoundRobin.java
@@ -0,0 +1,89 @@
+package com.circular_linked_list.round_robin_algo;
+
+public class RoundRobin {
+    public static int counter=0;
+
+    private int tq=3;
+
+
+    static class Node{
+        private int processId;
+        private int burstTime;
+        private int priority;
+        private Node next;
+        Node(int processId,int burstTime,int priority) {
+            this.processId = processId;
+            this.burstTime = burstTime;
+            this.priority = priority;
+            this.next = null;
+            counter++;
+
+        }
+    }
+    Node head=null;
+    Node temp=null;
+
+    // Methods for implementing Round Robin
+
+    public void addProcess(int processId,int burstTime,int priority){
+        Node newnode=new Node(processId,burstTime,priority);
+
+        if(head==null){
+            head=newnode;
+            temp=newnode;
+
+        }
+        else{
+            temp.next=newnode;
+            temp=newnode;
+            temp.next=head;
+        }
+
+    }
+    int time=0;
+
+public void startProcess(){
+        Node tail=head;
+        Node prev=head;
+        int t=counter;
+    System.out.println("\nTime Quantum is: "+tq);
+
+        while(counter!=0){
+            time+=(tail.burstTime)-tq;
+            tail.burstTime= (tail.burstTime)-tq;
+            if(tail.burstTime<=0){
+                System.out.println("\nDeleting the process: "+tail.processId);
+                counter--;
+                prev.next=tail.next;
+
+            }
+            else {
+
+
+                System.out.println("\nProcess Id: " + tail.processId + "\nBurst Time: " +tail.burstTime+"\nPriority: " + tail.priority);
+
+
+            }
+            prev=tail;
+            tail=tail.next;
+
+        }
+    System.out.println("\nAll processes are executed..........");
+    System.out.println("\nAverage time taken by process: "+(time/t));
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+

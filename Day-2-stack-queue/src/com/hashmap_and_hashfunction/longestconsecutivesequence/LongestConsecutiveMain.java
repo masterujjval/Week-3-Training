4e4029bebf52e7ec1373e59ad53e8cea5044dcd3
masterujjval <guptarickey3@gmail.com>
2025-01-30 18:34:16 +0530

    longest consecutive subsequence done

---
+++ Day-2-stack-queue/src/com/hashmap_and_hashfunction/longestconsecutivesequence/LongestConsecutiveMain.java
@@ -0,0 +1,11 @@
+package stackqueuehashsethashmap.hashmapshashfunctions.longestconsecutivesequence;
+
+public class LongestConsecutiveMain {
+    public static void main(String[] args) {
+        LongestConsective longest=new LongestConsective();
+
+ int arr[]={1,2,3,4,8,9,5};
+ longest.consective(arr);
+ longest.maxConsecutive();
+    }
+}

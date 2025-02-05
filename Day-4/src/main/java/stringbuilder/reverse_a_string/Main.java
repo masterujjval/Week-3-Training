b230e3d385b97a1dd55757f2f2f57c4e0b045f44
masterujjval <guptarickey3@gmail.com>
2025-02-05 13:24:16 +0530

    [Ujjwal Gupta] Add: reverse a string using stringbuilder

---
+++ Day-4/src/main/java/stringbuilder/reverse_a_string/Main.java
@@ -0,0 +1,9 @@
+package stringbuilder.reverse_a_string;
+
+public class Main {
+    public static void main(String[] args) {
+        ReverseString obj=new ReverseString();
+        String s=obj.reverse("Ujjwal Gupta");
+        System.out.println("The reverses string is: "+s);
+    }
+}

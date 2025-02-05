1fa07608fb06d414c609b498e8555f2ecb713d26
masterujjval <guptarickey3@gmail.com>
2025-02-05 13:26:05 +0530

    [Ujjwal Gupta] Add: Test cases for Stringbuilder problems

---
+++ Day-4/src/test/java/StringBuilder_Test/RemoveDuplicates_Test.java
@@ -0,0 +1,12 @@
+package StringBuilder_Test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import stringbuilder.remove_duplicates.RemoveDuplicates;
+
+public class RemoveDuplicates_Test {
+    @Test
+    public void tester(){
+        Assert.assertEquals("auwjl", RemoveDuplicates.remove("uuujjjjjwwwwaaaallll"));
+    }
+}
---
+++ Day-4/src/test/java/StringBuilder_Test/ReverseStringTest.java
@@ -0,0 +1,13 @@
+package StringBuilder_Test;
+import org.junit.Assert;
+import org.junit.Test;
+import stringbuilder.reverse_a_string.ReverseString;
+
+public class ReverseStringTest {
+@Test
+public void testing(){
+    Assert.assertEquals("lawjju", ReverseString.reverse("ujjwal"));
+}
+
+
+}

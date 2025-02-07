2a0ac11223c82a4348adc220dc73129509515b8f
masterujjval <guptarickey3@gmail.com>
2025-02-07 14:49:26 +0530

    [Ujjwal Gupta] Modified: String BUilder reverse a string

--- Day-4/src/main/java/stringbuilder/reverse_a_string/ReverseString.java
+++ Day-4/src/main/java/stringbuilder/reverse_a_string/ReverseString.java
@@ -1,15 +1,15 @@
-package stringbuilder.reverse_a_string;
+    package stringbuilder.reverse_a_string;
 
 
 
-public class ReverseString {
+    public class ReverseString {
 
-    public static   String reverse(String s){
-        StringBuilder ans=new StringBuilder(s);
-        return ans.reverse().toString();
+        public static   String reverse(String s){
+            StringBuilder ans=new StringBuilder(s);
+            return ans.reverse().toString();
+
+        }
+
+
 
     }
-
-
-
-}
